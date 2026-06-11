c5b88c41779d6180ea5feda0b775686f667e886f
PhilippJuepp <philippjueppner@web.de>
2026-06-11 16:09:22 +0200

    Add logo asset and header to DownloadBar

--- src/components/DownloadBar.tsx
+++ src/components/DownloadBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import icon from "../assets/Grabberty_Logo.svg"
 
 function App() {
   const [url, setUrl] = useState("")
@@ -7,9 +8,21 @@ function App() {
   return (
     <div className="w-full h-screen bg-[#0E0E0F] flex items-center justify-center">
 
-      <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-col items-center gap-14">
 
-        
+        <div className="flex flex-col items-center text-center gap-3">
+
+          <img src={icon} className="w-12 h-12 mb-3" />
+
+          <h1 className="text-white text-4xl font-bold">
+            Grabberty
+          </h1>
+
+          <p className="text-white">
+            Fast, simple and high-quality self-hosted downloader
+          </p>
+        </div>
+
         <div className="w-[700px] bg-[#1B1B1D] p-4 rounded-[30px] border-[2px] border-zinc-800 flex items-center gap-4">
 
           <div className="flex-1 h-[50px] bg-[#242427] rounded-[15px] flex items-center">
